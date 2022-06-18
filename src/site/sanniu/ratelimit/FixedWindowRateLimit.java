bb3e20b8c341e7f1d438d84abafea3d749f820b4
sanniu <1296646584@qq.com>
2022-06-18 23:29:24 +0800

    add ratelimit

---
+++ src/site/sanniu/ratelimit/FixedWindowRateLimit.java
@@ -0,0 +1,8 @@
+package site.sanniu.ratelimit;
+
+/**
+ * Created by sanniu on 2022/6/18 23:26
+ */
+public class FixedWindowRateLimit {
+
+}
---
+++ src/site/sanniu/ratelimit/TokenBucketRateLimit.java
@@ -0,0 +1,7 @@
+package site.sanniu.ratelimit;
+
+/**
+ * Created by sanniu on 2022/6/18 23:27
+ */
+public class TokenBucketRateLimit {
+}
